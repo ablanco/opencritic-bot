956281e4f4b7caa55ec90159cc3caa322f96d7a0
Alejandro Blanco <alejandro.b.e@gmail.com>
2023-12-30 18:04:07 +0100

    Bot scaffold

---
+++ index.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2023 Alejandro Blanco <alejandro.b.e@gmail.com>
+// MIT License
+
+import { Telegraf } from 'telegraf';
+import Settings from './src/settings.js';
+
+const bot = new Telegraf(Settings.token);
+
+const helpHTML = [
+    'This bot is meant to be use in inline mode.',
+    'Call it in any conversation with @opencriticbot',
+].join('\n');
+
+bot.command('start', (ctx) => {
+    ctx.replyWithHTML(helpHTML);
+});
+bot.command('help', (ctx) => {
+    ctx.replyWithHTML(helpHTML);
+});
+
+bot.on('inline_query', async (ctx) => {
+    const query = ctx.inlineQuery.query;
+
+    if (query) {
+        console.log(query);
+    }
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ src/settings.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2023 Alejandro Blanco <alejandro.b.e@gmail.com>
+// MIT License
+
+export default {
+    token: '',
+};
